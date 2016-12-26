07e510be3debaf7bf953fdb8ddf370e3ccec545b
Malik K <koljenovic@gmail.com>
2016-12-26 23:21:50 +0100

    Package fix.

--- SSO/SSO.WCFService/SSO.WCFService/ServiceInterfaces/IAccountManagement.cs
+++ SSO/SSO.WCFService/SSO.WCFService/ServiceInterfaces/IAccountManagement.cs
@@ -7,7 +7,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-namespace SSO.WCFService.BusinessLogic
+namespace SSO.WCFService.ServiceInterfaces
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAccountManagement" in both code and config file together.
     [ServiceContract]
