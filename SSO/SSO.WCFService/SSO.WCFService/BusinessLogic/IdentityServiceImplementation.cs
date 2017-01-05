3c25f079988f9e4316782d43e632feb3575515d6
SumejjaPorca <sumejja93@gmail.com>
2017-01-05 02:07:03 +0100

    authenticate method transported to authprovider. IsAdmin method: authProvider + RoleProvider

--- SSO/SSO.WCFService/SSO.WCFService/BusinessLogic/IdentityServiceImplementation.cs
+++ SSO/SSO.WCFService/SSO.WCFService/BusinessLogic/IdentityServiceImplementation.cs
@@ -4,49 +4,23 @@ using System.Linq;
 using System.Web;
 using SSO.WCFService.DataContracts;
 using System.Data.Entity;
+using SSO.WCFService.Helpers;
 
 namespace SSO.WCFService.BusinessLogic
 {
     public class IdentityServiceImplementation
     {
         private SSOContext _db;
+        private AuthProvider _authProvider;
         public IdentityServiceImplementation(SSOContext db)
         {
             _db = db;
+            _authProvider = new AuthProvider(_db);
         }
         public AuthResponse Auth(string token)
         {
-            var claim = _db.Claims
-                .SingleOrDefault(c => c.Valid.Equals("1") && c.Token.Equals(token));
-            if (claim == null)
-            {
-                // or return false
-                throw new Exceptions.WrongOrExpiredToken();
-            }
-
-            // Check expiration
-            var timeSpan = DateTime.Now.Subtract(claim.Created);
-            if(timeSpan.TotalSeconds > 60 * 60 * 24) // 24h
-            {
-                claim.Valid = "0";
-                _db.SaveChanges();
-                throw new Exceptions.WrongOrExpiredToken();
-            }
-
-
-            var user = _db.Users.Where(u => u.ID == claim.UserID).ToList()
-                                    .Select(u => new {
-                                        UserId = u.ID,
-                                        Username = u.Username,
-                                        Info = _db.UserInfoes.FirstOrDefault(i => i.UserID == u.ID),
-                                        Roles = _db.ManageRoles.Include(mr => mr.Role)
-                                                    .Where(mr => mr.UserID == u.ID)
-                                                    .Select(mr => mr.Role.Name)
-                                    }).FirstOrDefault();
-            /*_db.ManageRoles.Include(mr => mr.Role)
-                                                    .Where(mr => mr.UserID == u.ID)
-                                                    .Select(mr => mr.Role.Name)*/
-            if(user == null)
+            var user = _authProvider.AuthenticateByToken(token);
+            if (user == null)
             {
                 throw new Exception("This shouldn happen.");
             }
